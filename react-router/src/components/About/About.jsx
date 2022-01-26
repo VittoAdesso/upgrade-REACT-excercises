cde9610d47ca48c6fff2559c10ddfdd9bd2c6b47
Vittorio Adesso <adessovittorio8@gmail.com>
2022-01-26 22:38:24 +0100

    Complite get api blog type

--- react-router/src/components/About/About.jsx
+++ react-router/src/components/About/About.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function About() {
-  return (
-      <h2> About Page</h2>
+
+
+
+  const [ user, setUser ] = useState([]); // será igual a un array 
+
+    //uso el hook useefecct que siempre recibe 2 parámetros
+    // uso éste hook por código asyncrono, código secundario aaparte 
+    useEffect(() => { // uso ésto para hacer petición 
+
+        // pido info a la API
+        fetch("https://jsonplaceholder.typicode.com/comments")
+        .then((res) => res.json())
+        .then((json) => setUser(json))
+    }, []); 
+
+// aquí retorno un mapeo de los datos que pido a la api con el fecth 
+  return ( 
+      
+      <> 
+      {/* sino englobo el contenido en una etiqueta estilo ésta que es ciega a la web, me da error el map */}
+      
+
+  { user.map((item) => 
+
+        <div key={ item.id} >
+        <h3> {item.name} </h3>
+        <p> {item.body} </p>
+        </div>
+    
+    )}
+    </>
   );
-}
+};
+
+
+
--- react-router/src/components/Home/Home.jsx
+++ react-router/src/components/Home/Home.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 
 const Home = () => {
-  return (
-      
-        <h2>Home </h2>
-    );
+
+  // creo variables, y estado 
+
+const [ user, setUser ] = useState([]); // será igual a un array 
+
+//uso el hook useefecct que siempre recibe 2 parámetros
+// uso éste hook por código asyncrono, código secundario aaparte 
+useEffect(() => { // uso ésto para hacer petición 
+
+    // pido info a la API
+    fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((res) => res.json())
+    .then((json) => setUser(json))
+}, []); 
+
+// aquí retorno un mapeo de los datos que pido a la api con el fecth 
+return ( 
+  
+  <> 
+  {/* sino englobo el contenido en una etiqueta estilo ésta que es ciega a la web, me da error el map */}
+  
+
+{ user.map((item) => 
+
+    <div key={ item.id} >
+    <h3> {item.title} </h3>
+    <img src= { item.url } / >
+    </div>
+
+)}
+</>
+);
 };
 
 export default Home;
